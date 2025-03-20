d7ec5b602407d978b753977ba5eec66790ee2224
WillPrata <wilson.prata@soulasalle.com.br>
2025-03-20 19:52:10 -0300

    EX 3.C

---
+++ EX 3.C
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class CalculadoraSimples {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o primeiro número: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Digite o segundo número: ");
+        double num2 = scanner.nextDouble();
+
+        System.out.print("Escolha a operação (+, -, *, /): ");
+        char operacao = scanner.next().charAt(0);
+
+        switch (operacao) {
+            case '+':
+                System.out.println("Resultado: " + (num1 + num2));
+                break;
+            case '-':
+                System.out.println("Resultado: " + (num1 - num2));
+                break;
+            case '*':
+                System.out.println("Resultado: " + (num1 * num2));
+                break;
+            case '/':
+                if (num2 != 0) {
+                    System.out.println("Resultado: " + (num1 / num2));
+                } else {
+                    System.out.println("Erro: Divisão por zero!");
+                }
+                break;
+            default:
+                System.out.println("Operação inválida.");
+                break;
+        }
+
+        scanner.close();
+    }
+}
